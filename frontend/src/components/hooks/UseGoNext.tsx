a372be6c4cb30ff2c251293e30b05c89eba07d5d
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-15 18:03:52 +0300

    last commit

--- frontend/src/components/hooks/UseGoNext.tsx
+++ frontend/src/components/hooks/UseGoNext.tsx
@@ -3,13 +3,16 @@ import { useNavigatePages } from "./UseNavigatePages";
 
 export const useGoNext = () => {
   const navigate = useNavigate();
-  const { pages, currentIndex } = useNavigatePages();
+  // const { pages, currentIndex } = useNavigatePages();
+
+  //GO NEXT mantığı yok diğer sayfalara basmadan geçemezsin
 
   const goNext = () => {
-    if (currentIndex == 0) return; //kullanıcı giriş yapmadan başka sayafaya ilerleyemez
-    if (currentIndex < pages.length - 1) {
-      navigate({ to: pages[currentIndex + 1] });
-    }
+    return;
+    // if (currentIndex == 0) return; //kullanıcı giriş yapmadan başka sayafaya ilerleyemez
+    // if (currentIndex < pages.length - 1) {
+    //   navigate({ to: pages[currentIndex + 1] });
+    // }
   };
 
   return goNext;
--- frontend/src/components/hooks/UseGoPrev.tsx
+++ frontend/src/components/hooks/UseGoPrev.tsx
@@ -7,16 +7,26 @@ export const useGoPrev = () => {
   const { pages, currentIndex } = useNavigatePages();
   const logout = useLogOut();
   const goPrev = () => {
-    //Eğer /a sayfasındaysam ve geri gidiceksem kullanıcı giriş sayfasına çıkış yapmak isteyip istemeyeceğinin mesajını göderiyorum okeylerse çıkış  yapıyor.
-    if (currentIndex == 1) {
-      const isConfirmed = confirm("çıkış mı yapmak istiyorsunuz?");
-      //çıkış işlemi
-      if (isConfirmed) {
-        const success = logout();
-        alert(success ? "çıkış başarılı" : "çıkış başarısız");
-      }
-      //eğer hayır ise hiçbir şey yapma
-      return;
+    switch (currentIndex) {
+      case 3:
+        navigate({ to: "/a" });
+        break;
+      case 2:
+        navigate({ to: "/a" });
+        break;
+      case 1:
+        //Eğer /a sayfasındaysam ve geri gidiceksem kullanıcı giriş sayfasına çıkış yapmak isteyip istemeyeceğinin mesajını göderiyorum okeylerse çıkış  yapıyor.
+        if (currentIndex == 1) {
+          const isConfirmed = confirm("çıkış mı yapmak istiyorsunuz?");
+          //çıkış işlemi
+          if (isConfirmed) {
+            const success = logout();
+            alert(success ? "çıkış başarılı" : "çıkış başarısız");
+          }
+          // eğer hayır ise hiçbir şey yapma
+
+          break;
+        }
     }
 
     if (currentIndex > 0) {
